e5aefbe9a0efa9ca7029aa5b9447980603d9d727
Eduar Apaza <eapaza@learnsoft.com>
2024-05-16 21:36:04 -0500

    Pattern Design for Fetch Data

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -3,11 +3,13 @@ import { useFetchUsers } from "../hooks/useFetchUsers";
 // Single Reponsibility
 
 export default function UsersList() {
-  const [users] = useFetchUsers();
+  const [users, loading, error] = useFetchUsers();
 
   return (
     <div>
       <h2>Users List</h2>
+      {loading ? <h3>Loading...</h3> : null}
+      {error !== "" ? <p>{error}</p> : null}
       <ul>
         {users.map((user) => (
           <li>{user.name}</li>
--- src/hooks/useFetchUsers.tsx
+++ src/hooks/useFetchUsers.tsx
@@ -2,18 +2,31 @@ import { useEffect, useState } from "react";
 
 function useFetchUsers() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const handleFetch = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const result = await response.json();
-    setUsers(result.results);
+    try {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const result = await response.json();
+      if (result && result.results) {
+        setUsers(result.results);
+      } else {
+        setError("Something went wrong");
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log("error");
+      setError("Error Message");
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     handleFetch();
   }, []);
 
-  return [users];
+  return [users, loading, error];
 }
 
 export { useFetchUsers };
